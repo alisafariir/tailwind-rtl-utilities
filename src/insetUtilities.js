c774c38b281c13d226264facae644a300c4b13b3
asafari72 <asafari72@gmail.com>
2022-04-10 12:50:33 +0430

    rtl fix

--- src/insetUtilities.js
+++ src/insetUtilities.js
@@ -3,19 +3,33 @@ const nameClass = require('./util/nameClass.js');
 module.exports = (theme) => {
   const generators = [
     ([modifier, size]) => ({
-      ['[dir="rtl"] ' + nameClass('start', modifier)]: {
+      [nameClass('start', modifier)]: {
         right: size,
       },
-      ['[dir="rtl"] ' + nameClass('end', modifier)]: {
+      [nameClass('end', modifier)]: {
         left: size,
       },
-      ['[dir="ltr"] ' + nameClass('end', modifier)]: {
+      [nameClass('end', modifier)]: {
         right: size,
       },
-      ['[dir="ltr"] ' + nameClass('start', modifier)]: {
+      [nameClass('start', modifier)]: {
         left: size,
+      },
+      [nameClass('-start', modifier)]: {
+        right: -size,
+      },
+      [nameClass('-end', modifier)]: {
+        left: -size,
+      },
+      [nameClass('-end', modifier)]: {
+        right: -size,
+      },
+      [nameClass('-start', modifier)]: {
+        left: -size,
       },
     }),
   ];
-  return generators.flatMap(generator => Object.entries(theme('inset')).flatMap(generator));
+  return generators.flatMap((generator) =>
+    Object.entries(theme('inset')).flatMap(generator)
+  );
 };
